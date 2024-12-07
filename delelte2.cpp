2dc65b09502debfc14d6c509b14d607e6f05a6d1
wekkend <143948324+wekkend@users.noreply.github.com>
2024-12-07 10:22:41 +0700

    Add files via upload

--- delelte2.cpp
+++ delelte2.cpp
@@ -1,221 +1,356 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
+#include<iostream>
+#include<string>
+#include<string.h>
+
 using namespace std;
 
-// Định nghĩa số nhân viên tối đa
-const int MAX_NHANVIEN = 100;
+struct Date {
+    int ngay;
+    int thang;
+    int nam;
+};
 
-// Lớp ConNguoi
-class ConNguoi {
-protected:
-    string maSo;
-    string hoTen;
-    string ngaySinh;
+class NuocGiaiKhat {
+private:
+    string maNuoc;
+    string tenNuoc;
+    Date hanSuDung;
+    float trongLuong;
 
 public:
-    ConNguoi() {}
+    NuocGiaiKhat() {};
+
+    string getMaNuoc() { return maNuoc; }
+    string getTenNuoc() { return tenNuoc; }
+    Date getHanSuDung() { return hanSuDung; }
+    float getTrongLuong() { return trongLuong; }
+
+    void setMaNuoc(string ma) { maNuoc = ma; }
 
     void nhap() {
-        cout << "Nhap ma so: ";
-        cin >> maSo;
+        cout << "Nhap ten nuoc" << endl;
+        getline(cin, tenNuoc);
+        cout << "Nhap han su dung" << endl;
+        cin >> hanSuDung.ngay >> hanSuDung.thang >> hanSuDung.nam;
+        cout << "Nhap trong luong " << endl;
+        cin >> trongLuong;
+    }
+
+    void xuat() {
+        cout << "--------------------------------------------------------" << endl;
+        cout << "Thong tin san pham:" << endl;
+        cout << "Ma nuoc: " << maNuoc << endl;
+        cout << "Ten Nuoc: " << tenNuoc << endl;
+        cout << "Han SU Dung: " << hanSuDung.ngay << "/" << hanSuDung.thang << "/" << hanSuDung.nam << endl;
+        cout << "TrongLuong: " << trongLuong << endl;
+    }
+
+    float getGiaNuocNgot() {
+        return trongLuong * 300;
+    }
+};
+
+class NuocNgot : public NuocGiaiKhat {
+private:
+    float doNgot;
+    static int demNuocNgot;  // Biến static để đếm số lượng nuoc ngọt
+
+public:
+    NuocNgot() {
+        demNuocNgot++;  // Tăng biến đếm mỗi khi tạo đối tượng mới
+        setMaNuoc("NN" + to_string(100 + demNuocNgot));  // Tạo mã nuoc ngọt tự động
+    }
+
+    float getDoNgot() { return doNgot; }
+
+    void nhap() {
+        NuocGiaiKhat::nhap();
+        cout << "Nhap do ngot " << endl;
+        cin >> doNgot;
+    }
+
+    void xuat() {
+        NuocGiaiKhat::xuat();
+        cout << "Do ngot: " << doNgot << endl;
+        cout << "Co gia tien: " << getGiaNuocNgot() << endl;
+    }
+
+    static int getDemNuocNgot() {
+        return demNuocNgot;
+    }
+};
+
+// Khởi tạo static biến
+int NuocNgot::demNuocNgot = 0;
+
+class NuocCoGas : public NuocGiaiKhat {
+private:
+    string xuatXu;
+    static int demNuocCoGas;  // Biến static để đếm số lượng nuoc có gas
+
+public:
+    NuocCoGas() {
+        demNuocCoGas++;  // Tăng biến đếm mỗi khi tạo đối tượng mới
+        setMaNuoc("NG" + to_string(100 + demNuocCoGas));  // Tạo mã nuoc có gas tự động
+    }
+
+    string getXuatXu() { return xuatXu; }
+
+    void nhap() {
+        NuocGiaiKhat::nhap();
         cin.ignore();
-        cout << "Nhap ho ten: ";
-        getline(cin, hoTen);
-        cout << "Nhap ngay sinh (dd/mm/yyyy): ";
-        cin >> ngaySinh;
+        cout << "Nhap xuat xu:" << endl;
+        getline(cin, xuatXu);
     }
 
+    float getGiaNuocCoGas() {
+        if (xuatXu == "Chau A") {
+            return getGiaNuocNgot() * 0.95;
+        } else if (xuatXu == "Chau Au") {
+            return getGiaNuocNgot() * 1.5;
+        } else {
+            cout << "Xuat xu khong hop le" << endl;
+            return 0;
+        }
+    }
     void xuat() {
-        cout << "Ma so: " << maSo << ", Ho ten: " << hoTen << ", Ngay sinh: " << ngaySinh;
+        NuocGiaiKhat::xuat();
+        cout << "Xuat xu: " << xuatXu << endl;
+        cout << "Co gia tien la: " << getGiaNuocCoGas() << endl;
     }
 
-    string getMaSo() { return maSo; }
+    static int getDemNuocCoGas() {
+        return demNuocCoGas;
+    }
+    void thongKeDanhSach(NuocCoGas a[],int n){};
 };
 
-// Lớp NhanVien kế thừa từ ConNguoi
-class NhanVien : public ConNguoi {
-private:
-    double luong;
-    string ngayNhanViec;
+// Khởi tạo static biến
+int NuocCoGas::demNuocCoGas = 0;
 
-public:
-    void nhap() {
-        ConNguoi::nhap();
-        cout << "Nhap luong: ";
-        cin >> luong;
-        cout << "Nhap ngay nhan viec (dd/mm/yyyy): ";
-        cin >> ngayNhanViec;
+void nhapDSNuocNgot(NuocNgot a[], int &n) {
+    a[n].nhap();  // Gọi nhap() cho từng đối tượng trong mảng
+    n++;  // Tăng số lượng nước ngọt đã nhập
+}
+
+void nhapDSNuocCoGas(NuocCoGas a[], int &n) {
+    a[n].nhap();  // Gọi nhap() cho từng đối tượng trong mảng
+    n++;  // Tăng số lượng nước có gas đã nhập
+}
+
+void xuatDSNuocNgot(NuocNgot a[], int n) {
+    cout << "Danh sach san pham nuoc ngot la:" << endl;
+    for (int i = 0; i < n; i++) {
+        a[i].xuat();  // Gọi xuat() cho từng đối tượng trong mảng
     }
+}
 
-    void xuat() {
-        ConNguoi::xuat();
-        cout << ", Luong: " << fixed << setprecision(2) << luong << ", Ngay nhan viec: " << ngayNhanViec << endl;
+void xuatDSNuocCoGas(NuocCoGas a[], int n) {
+    cout << "Danh sach san pham nuoc co gas la: " << endl;
+    for (int i = 0; i < n; i++) {
+        a[i].xuat();  // Gọi xuat() cho từng đối tượng trong mảng
     }
+}
+void tinhTongTien(NuocGiaiKhat a[],NuocCoGas b[], int n) {
+    double tongTien=0;
+    for(int i=0;i<n;i++){
+        tongTien+=a[i].getGiaNuocNgot()+b[i].getGiaNuocCoGas();
+    }
+    cout<<"Tong tien phai tra la: "<<tongTien<<endl;
+}
+void timKiemTheoHanSD(NuocGiaiKhat a[], int n) {
+    cout << "Nhap han su dung can tim kiem: " << endl;
+    Date hanSD;
+    bool found = false;
+    cin >> hanSD.ngay >> hanSD.thang >> hanSD.nam;
 
-    double getLuong() { return luong; }
-    string getNgayNhanViec() { return ngayNhanViec; }
-
-    void setLuong(double l) { luong = l; }
-    void setNgayNhanViec(string date) { ngayNhanViec = date; }
-};
-
-// Lớp quản lý nhân viên
-class QLNhanVien {
-private:
-    NhanVien danhSach[MAX_NHANVIEN];
-    int soLuong;
-
-public:
-    QLNhanVien() { soLuong = 0; }
-
-    void nhapDanhSach() {
-        cout << "Nhap so luong nhan vien: ";
-        cin >> soLuong;
-        for (int i = 0; i < soLuong; i++) {
-            cout << "\nNhap thong tin nhan vien thu " << i + 1 << ":\n";
-            danhSach[i].nhap();
+    cout << "Danh sach san pham co han su dung " << hanSD.ngay << "/" << hanSD.thang << "/" << hanSD.nam << " la:" << endl;
+    
+    // Duyệt qua danh sách sản phẩm và so sánh hạn sử dụng
+    for (int i = 0; i < n; i++) {
+        Date temp = a[i].getHanSuDung();  // Lấy hạn sử dụng của sản phẩm
+        if (temp.ngay == hanSD.ngay && temp.thang == hanSD.thang && temp.nam == hanSD.nam) {
+            a[i].xuat();  // Xuất thông tin sản phẩm nếu trùng khớp
+            found = true;
         }
     }
+    
+    // Nếu không tìm thấy sản phẩm nào, thông báo
+    if (!found) {
+        cout << "Khong tim thay san pham co han su dung " << hanSD.ngay << "/" << hanSD.thang << "/" << hanSD.nam << endl;
+    }
+}
+// void timKiemNuocHetHan8Ngay(NuocGiaiKhat ds[], int n) {
+//     time_t now = time(0);
+//     tm* ltm = localtime(&now);
+//     int ngayHienTai = ltm->tm_mday;
+//     int thangHienTai = ltm->tm_mon + 1;
+//     int namHienTai = ltm->tm_year + 1900;
 
-    void inDanhSach() {
-        cout << "\nDanh sach nhan vien:\n";
-        for (int i = 0; i < soLuong; i++) {
-            danhSach[i].xuat();
+//     for (int i = 0; i < n; i++) {
+//         int soNgayConLai = (ds[i].getHanSuDung().nam - namHienTai) * 365 +
+//                            (ds[i].getHanSuDung().thang - thangHienTai) * 30 +
+//                            (ds[i].getHanSuDung().ngay - ngayHienTai);
+//         if (soNgayConLai < 8) {
+//             ds[i].xuat();
+//         }
+//     }
+// }
+
+// void timKiemNuocCoGasHetHanThang10_2024(NuocCoGas ds[], int n) {
+//     for (int i = 0; i < n; i++) {
+//         if (ds[i].getHanSuDung().nam == 2024 && ds[i].getHanSuDung().thang == 10) {
+//             ds[i].xuat();
+//         }
+//     }
+// }
+
+// void timKiemNuocCoGasChauA(NuocCoGas ds[], int n) {
+//     for (int i = 0; i < n; i++) {
+//         if (ds[i].getXuatXu() == "Chau A" && ds[i].getTrongLuong() >= 1000) {
+//             ds[i].xuat();
+//         }
+//     }
+// }
+
+void tinmKiemNuocTheoTen(NuocGiaiKhat a[],int n){
+    bool found = false;
+    string stringF;
+    cout<<"Nhap ten can tim kiem:"<<endl;
+    getline(cin,stringF);
+    cin.ignore();
+    for(int i = 0; i<n;i++){
+        if(a[i].getTenNuoc().find(stringF) != string::npos)
+        {
+            a[i].xuat();
+            found = true;
         }
     }
-
-    void sapXepTheoLuongGiamDan() {
-        for (int i = 0; i < soLuong - 1; i++) {
-            for (int j = i + 1; j < soLuong; j++) {
-                if (danhSach[i].getLuong() < danhSach[j].getLuong()) {
-                    swap(danhSach[i], danhSach[j]);
-                }
+    if(!found){
+        cout<<"Khong tim thay san pham co ten "<<stringF<<endl;
+    }
+}
+void thongKeDanhSach(NuocCoGas a[],int n){
+    int soLuong=0;
+    float tongTien=0;
+    for(int i=0;i<n;i++){
+        if(a[i].getXuatXu()=="Chau A"){
+            soLuong+=1;
+            tongTien+=a[i].getGiaNuocCoGas();
+            a[i].xuat();
+        }
+    }
+}
+void xoaNuocTheoMa(NuocGiaiKhat a[],int n){
+    string ma;
+    bool found= false;
+    cout<<"Nhap ma can xoa"<<endl;
+    getline(cin,ma);
+    cin.ignore();
+    for(int i=0;i<n;i++){
+        if(a[i].getMaNuoc()==ma){
+            found=true;
+            for(int j=i;j<n-1;j++){
+                a[j]=a[j+1];
             }
+            n--;
+            cout<<"Da xoa nuoc co ma "<<ma<<endl;
+            break;
         }
     }
-
-    void timNhanVien() {
-        string ma;
-        cout << "\nNhap ma so nhan vien can tim: ";
-        cin >> ma;
-        for (int i = 0; i < soLuong; i++) {
-            if (danhSach[i].getMaSo() == ma) {
-                danhSach[i].xuat();
-                return;
-            }
-        }
-        cout << "Khong tim thay nhan vien!\n";
+    if(!found){
+        cout<<"Khong tim thay ma nuoc "<<ma<<endl;
     }
+}
+// void xoaNuocTheoMa(NuocGiaiKhat ds[], int& n, const string& ma) {
+//     for (int i = 0; i < n; i++) {
+//         if (ds[i].getMaNuoc() == ma) {
+//             for (int j = i; j < n - 1; j++) {
+//                 ds[j] = ds[j + 1];
+//             }
+//             n--;
+//             cout << "Da xoa nuoc co ma " << ma << endl;
+//             return;
+//         }
+//     }
+//     cout << "Khong tim thay ma nuoc " << ma << endl;
+// }
 
-    void xuatNhanVienSauNgay() {
-        string ngay;
-        cout << "\nNhap ngay (dd/mm/yyyy): ";
-        cin >> ngay;
-        cout << "\nDanh sach nhan vien vao lam sau " << ngay << ":\n";
-        for (int i = 0; i < soLuong; i++) {
-            if (danhSach[i].getNgayNhanViec() > ngay) {  // So sánh chuỗi
-                danhSach[i].xuat();
-            }
-        }
-    }
-
-    void demNhanVienTheoNam() {
-        string nam;
-        cout << "\nNhap nam (yyyy): ";
-        cin >> nam;
-        int count = 0;
-        for (int i = 0; i < soLuong; i++) {
-            if (danhSach[i].getNgayNhanViec().substr(6, 4) == nam) {  // So sánh năm
-                count++;
-            }
-        }
-        cout << "So luong nhan vien vao lam trong nam " << nam << ": " << count << endl;
-    }
-
-    void suaNhanVien() {
-        string ma;
-        cout << "\nNhap ma so nhan vien can sua: ";
-        cin >> ma;
-        for (int i = 0; i < soLuong; i++) {
-            if (danhSach[i].getMaSo() == ma) {
-                cout << "Nhap thong tin moi cho nhan vien:\n";
-                danhSach[i].nhap();
-                return;
-            }
-        }
-        cout << "Khong tim thay nhan vien!\n";
-    }
-
-    void xoaNhanVien() {
-        string ma;
-        cout << "\nNhap ma so nhan vien can xoa: ";
-        cin >> ma;
-        for (int i = 0; i < soLuong; i++) {
-            if (danhSach[i].getMaSo() == ma) {
-                for (int j = i; j < soLuong - 1; j++) {
-                    danhSach[j] = danhSach[j + 1];
-                }
-                soLuong--;
-                cout << "Da xoa nhan vien.\n";
-                return;
-            }
-        }
-        cout << "Khong tim thay nhan vien!\n";
-    }
-};
-
-// Hàm menu
+// void sapXepGiaTien(NuocGiaiKhat ds[], int n) {
+//     for (int i = 0; i < n - 1; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             if (ds[i].getGiaNuoc() > ds[j].getGiaNuoc()) {
+//                 swap(ds[i], ds[j]);
+//             }
+//         }
+//     }
+// }
 int main() {
-    QLNhanVien ql;
+    NuocGiaiKhat a[100];
+    NuocNgot n[100];
+    NuocCoGas n1[100];
+    int demNuocNgot = 0;  // Biến đếm nước ngọt
+    int demNuocCoGas = 0; // Biến đếm nước có gas
     int choice;
-    do {
-        cout << "\n--- MENU QUAN LY NHAN VIEN ---\n";
-        cout << "1. Nhap danh sach nhan vien\n";
-        cout << "2. In danh sach nhan vien\n";
-        cout << "3. Sap xep danh sach theo luong giam dan\n";
-        cout << "4. Tim nhan vien theo ma so\n";
-        cout << "5. Xuat nhan vien vao lam sau ngay nhap vao\n";
-        cout << "6. Dem nhan vien vao lam theo nam\n";
-        cout << "7. Sua thong tin nhan vien theo ma so\n";
-        cout << "8. Xoa thong tin nhan vien theo ma so\n";
-        cout << "0. Thoat\n";
-        cout << "Lua chon cua ban: ";
-        cin >> choice;
 
-        switch (choice) {
-            case 1:
-                ql.nhapDanhSach();
-                break;
-            case 2:
-                ql.inDanhSach();
-                break;
-            case 3:
-                ql.sapXepTheoLuongGiamDan();
-                cout << "Da sap xep theo luong giam dan.\n";
-                break;
-            case 4:
-                ql.timNhanVien();
-                break;
-            case 5:
-                ql.xuatNhanVienSauNgay();
-                break;
-            case 6:
-                ql.demNhanVienTheoNam();
-                break;
-            case 7:
-                ql.suaNhanVien();
-                break;
-            case 8:
-                ql.xoaNhanVien();
-                break;
-            case 0:
-                cout << "Thoat chuong trinh.\n";
-                break;
-            default:
-                cout << "Lua chon khong hop le.\n";
+    // Menu và lựa chọn
+    while (true) {
+        cout << "\n---- MENU ----\n";
+        cout << "1. Nhap nuoc ngot\n";
+        cout << "2. Nhap nuoc co gas\n";
+        cout << "3. Xuat danh sach nuoc ngot\n";
+        cout << "4. Xuat danh sach nuoc co gas\n";
+        cout << "5.Tinh tong tien can cha \n";
+        cout << "6.Tim kiem theo han su dung\n";
+        cout << "0. Thoat\n";
+        cout << "Chon thao tac: ";
+        cin >> choice;
+        cin.ignore();  // Để tránh vấn đề khi dùng getline sau khi dùng cin
+
+        if (choice == 1) {
+            nhapDSNuocNgot(n, demNuocNgot);
+            cout << "Nuoc ngot da duoc nhap!\n";
         }
-    } while (choice != 0);
+        else if (choice == 2) {
+            nhapDSNuocCoGas(n1, demNuocCoGas);
+            cout << "Nuoc co gas da duoc nhap!\n";
+        }
+        else if (choice == 3) {
+            xuatDSNuocNgot(n, demNuocNgot);
+        }
+        else if (choice == 4) {
+            xuatDSNuocCoGas(n1, demNuocCoGas);
+        }
+        else if (choice == 5) {
+            cout<<"so tien phai tra la :"<<endl;
+            tinhTongTien(a,n1,demNuocCoGas+demNuocNgot); ///
+                  // Thoát khỏi vòng lặp khi chọn "Thoat"
+        } 
+        else if(choice == 6){
+            cout<<"Nhap han su dung can tim kiem cua nuoc ngot"<<endl;
+            timKiemTheoHanSD(n, demNuocNgot);
+            cout<<"Nhap han su dung can tim kiem cua nuoc co gas"<<endl;
+            timKiemTheoHanSD(n1, demNuocCoGas);
+            // timKiemTheoHanSD(a,demNuocCoGas+demNuocNgot);
+        }
+        else if(choice == 7){
+            cout<<"Nhap ten san pham can tim kiem cua nuoc ngot"<<endl;
+            tinmKiemNuocTheoTen(n, demNuocNgot);
+            cout<<"Nhap ten san pham can tim kiem cua nuoc co gas"<<endl;
+            tinmKiemNuocTheoTen(n1, demNuocCoGas);
+            // timKiemTheoHanSD(a,demNuocCoGas+demNuocNgot);
+        }
+         else if (choice == 8) {
+            cout<<"Thong ke nuoc co xuat xu chau a"<<endl;
+            thongKeDanhSach(n1, demNuocCoGas); 
+        }
+         else if (choice == 9) {
+            xoaNuocTheoMa(n1, demNuocCoGas); 
+        }
+        else {
+            cout << "Chon sai! Vui long chon lai.\n";
+        }
+    }
 
     return 0;
 }
